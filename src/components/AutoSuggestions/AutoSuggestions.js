0586baad13fa9571b44a1db47947f95f50add88f
ankitbhatia87 <ankit.bhatia87@gmail.com>
2021-04-27 00:01:15 +0530

    added isloading class and clickhandler on list item

--- src/components/AutoSuggestions/AutoSuggestions.js
+++ src/components/AutoSuggestions/AutoSuggestions.js
@@ -1,10 +1,13 @@
 import classes from './AutoSuggestions.module.css';
 
 const AutoSuggestions = (props) => {
+  const handleItemClick = e => {
+    props.onItemClick(e.currentTarget.innerText)
+  }
   return (
     <ul className={classes.SuggestionsWrap}>
       {props.selectedSuggestions.map((suggestion) => {
-        return <li className={classes.SuggestionsWrap_listItem} key={suggestion.id}>{suggestion.name}</li>;
+        return <li className={classes.SuggestionsWrap_listItem} onClick={handleItemClick} key={suggestion.id}>{suggestion.name}</li>;
       })}
     </ul>
   );
--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -4,7 +4,7 @@ const InputField = props => {
   const handleKeyup = (e) => {
       props.onInputValueUpdate(e.target.value);
   };
-  return <input type="text" className={classes.input} onKeyUp={handleKeyup} />;
+  return <input type="text" className={`${classes.input} ${props.isLoadingClass}`} onKeyUp={handleKeyup} />;
 };
 
 export default InputField;
